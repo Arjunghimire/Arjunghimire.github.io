0a976dd6e62d31e85365643afce9a2e9d07bb1c6
Arjun Ghimire <arjunghimire0714@gmail.com>
2019-09-08 16:45:25 +0545

    Fixed issues

---
+++ static/service-worker.js
@@ -0,0 +1,91 @@
+var cache_name = 'gih-cache';
+var cached_urls = [
+  "/offline.html",
+  "/index.html",
+  "/css/bootstrap.min.css",
+  "/css/font-awesome.min.css",
+  "/css/style.css",
+  "/css/timeline.css",
+  "/fonts/fontawesome-webfont.eot",
+  "/fonts/fontawesome-webfont.svg",
+  "/fonts/fontawesome-webfont.ttf",
+  "/fonts/fontawesome-webfont.woff",
+  "/fonts/fontawesome-webfont.woff2",
+  "/fonts/FontAwesome.otf",
+  "/js/bootstrap.min.js",
+  "/js/easing.js",
+  "/js/jquery-2.2.3.min.js",
+  "/js/menu.js",
+  "/js/move-top.js",
+  "/js/responsiveslides.min.js",
+  "/js/script.js",
+  "/js/scrolling-nav.js",
+  "/js/SmoothScroll.min.js",
+  "/js/timeline.js",
+  "/images/2.png",
+  "/images/banner.jpg",
+  "/images/close.png",
+  "/images/g2.png",
+  "/images/g3.png",
+  "/images/g5.png",
+  "/images/g6.png",
+  "/images/g7.png",
+  "/images/g8.png",
+  "/images/left1.png",
+  "/images/move-top.png",
+  "/images/right1.png",
+  "/images/s1.png",
+  "/images/s2.png",
+  "/images/s3.png",
+  "/images/s4.png",
+  "/images/s5.png",
+  "/images/s6.png"
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(cache_name)
+    .then(function (cache) {
+      return cache.addAll(cached_urls);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheName.startsWith('pages-cache-') && staticCacheName !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request).then(function (response) {
+        if (response.status === 404) {
+          return caches.match('index.html');
+        }
+        return caches.open(cached_urls).then(function (cache) {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+    }).catch(function (error) {
+      console.log('Error, ', error);
+      return caches.match('../offline.html');
+    })
+  );
+});
