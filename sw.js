dc70e76f5bdd80048c34caf388c2b61db9d82d5e
Arjun Ghimire <arjunghimire0714@gmail.com>
2019-09-08 16:20:22 +0545

    Fixed issues

--- sw.js
+++ sw.js
@@ -1,25 +1,27 @@
-self.addEventListener("install", function (event) {
+self.addEventListener("install", function(event) {
   event.waitUntil(preLoad());
 });
 
-var preLoad = function () {
+var preLoad = function() {
   console.log("Installing web app");
-  return caches.open("offline").then(function (cache) {
+  return caches.open("offline").then(function(cache) {
     console.log("caching index and important routes");
     return cache.addAll(["/css", "/fonts", "/", "/images", "/js"]);
   });
 };
 
-self.addEventListener("fetch", function (event) {
-  event.respondWith(checkResponse(event.request).catch(function () {
-    return returnFromCache(event.request);
-  }));
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    checkResponse(event.request).catch(function() {
+      return returnFromCache(event.request);
+    })
+  );
   event.waitUntil(addToCache(event.request));
 });
 
-var checkResponse = function (request) {
-  return new Promise(function (fulfill, reject) {
-    fetch(request).then(function (response) {
+var checkResponse = function(request) {
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response) {
       if (response.status !== 404) {
         fulfill(response);
       } else {
@@ -29,18 +31,18 @@ var checkResponse = function (request) {
   });
 };
 
-var addToCache = function (request) {
-  return caches.open("offline").then(function (cache) {
-    return fetch(request).then(function (response) {
+var addToCache = function(request) {
+  return caches.open("offline").then(function(cache) {
+    return fetch(request).then(function(response) {
       console.log(response.url + " was cached");
       return cache.put(request, response);
     });
   });
 };
 
-var returnFromCache = function (request) {
-  return caches.open("offline").then(function (cache) {
-    return cache.match(request).then(function (matching) {
+var returnFromCache = function(request) {
+  return caches.open("offline").then(function(cache) {
+    return cache.match(request).then(function(matching) {
       if (!matching || matching.status == 404) {
         return cache.match("index.html");
       } else {
